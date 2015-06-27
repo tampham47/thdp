893890a1ec583b2e9494a30a0bfb0e0b5da3c39b
Tam Pham <tampham47@live.com>
2015-06-28 01:29:45 +0700

    show triethocduongpho page

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
-// var read = require('easy-read');
+var read = require('node-readability');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
 
-  // read('http://www.triethocduongpho.com/2015/06/05/vi-sao-chu-nghia-nha-nuoc-that-bai/', function(result) {
-  //   // console.log(result.title, result.content);
-  //   res.send(result.content);
-  // });
+  var url = 'http://www.triethocduongpho.com/2015/06/24/ban-ve-lam-dung-danh-xung/';
+  read(url, {}, function(err, article, meta) {
+    if (err) {
+      res.send('err');
+      console.log(err);
+    } else {
+      res.send(article.content);
+      // res.send(article.html);
+      article.close();
+    }
+  });
 });
 
 module.exports = router;
