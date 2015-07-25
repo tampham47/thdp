0669d63e82f3d6a19720926573803fefc5abb470
Tam Pham <tampham47@live.com>
2015-07-25 10:53:16 +0700

    add crawler

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,32 @@
-var express = require('express');
-var router = express.Router();
-// var read = require('node-readability');
-// var jsdom = require('jsdom');
-var request = require('request');
-var cheerio = require('cheerio');
+
+var Crawler = require('crawler');
+var Express = require('express');
+
+var router = Express.Router();
+var url = require('url');
+var craw = new Crawler({
+  maxConnections : 10,
+  // This will be called for each crawled page
+  callback : function (error, result, $) {
+    // $ is Cheerio by default
+    //a lean implementation of core jQuery designed specifically for the server
+    $('a').each(function(index, a) {
+      var toQueueUrl = $(a).attr('href');
+      craw.queue(toQueueUrl);
+    });
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var url = 'http://www.triethocduongpho.com/2015/06/24/ban-ve-lam-dung-danh-xung/';
-  // var url = 'http://genk.vn/kham-pha/nhung-buc-anh-dang-kinh-ngac-cho-thay-tinh-trang-o-nhiem-tai-trung-quoc-20150202143233258.chn';
-  // read(url, {}, function(err, article, meta) {
-  //   if (err) {
-  //     res.send('err');
-  //     console.log(err);
-  //   } else {
-  //     res.send(article.content);
-  //     // res.send(article.html);
-  //     article.close();
-  //   }
-  // });
-
-  // jsdom.env(url, ['http://code.jquery.com/jquery.js'], function(err, window) {
-  //   if (err) {
-  //     console.log('err', err);
-  //     res.send(err);
-  //   } else {
-  //     res.send(window.$('body'));
-  //   }
-  // });
-
-  request(url, function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      // console.log(html);
-      var $query = cheerio.load(html);
-      $query = $query('main').remove();
-
-      res.send($query.html());
+  craw.queue([{
+    uri: 'http://www.triethocduongpho.com/2015/06/24/ban-ve-lam-dung-danh-xung/',
+    callback: function(err, result, $) {
+      var $content = $('main').remove();
+      res.send($content.html());
     }
-  });
+  }]);
 });
 
 module.exports = router;
